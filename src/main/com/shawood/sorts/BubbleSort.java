f7b388e54bd0e04e156b22b68b11318e1f6849b8
xiaozhihao <xiao.zhihao@byxgame.com>
2021-08-03 11:15:06 +0800

    增加冒泡和选择排序

--- src/main/com/shawood/sorts/BubbleSort.java
+++ src/main/com/shawood/sorts/BubbleSort.java
@@ -1,2 +1,40 @@
-package main.com.shawood.sorts;public class BubbleSort {
+package main.com.shawood.sorts;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @author shawood
+ * 冒泡排序
+ * 仅用于自己学习使用
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * @author 菜鸟教程
+     */
+    public int[] Solution_1(int[] nums){
+        // 对arr进行拷贝，不改变参数内容
+        int[] res = Arrays.copyOf(nums,nums.length);
+        for (int i=1; i < nums.length; i ++){
+            // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
+            boolean flag = true;
+            for (int j = 0; j < nums.length -i; j ++){
+                if (res[j] > res[j + 1]){
+                    int tmp = res[j];
+                    res[j] = res[j+1];
+                    res[j+1] = tmp;
+
+                    flag = false;
+                }
+            }
+            if (flag){
+                break;
+            }
+        }
+        return res;
+    }
 }
--- src/main/com/shawood/sorts/SelectionSort.java
+++ src/main/com/shawood/sorts/SelectionSort.java
@@ -1,2 +1,42 @@
-package main.com.shawood.sorts;public class SelectionSort {
+package main.com.shawood.sorts;
+
+import java.util.Arrays;
+
+/**
+ * @author shawood
+ * 选择排序
+ *
+ */
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {1,4,2,5,7};
+        SelectionSort ss = new SelectionSort();
+        System.out.println("res: " + Arrays.toString(ss.Solution_1(nums)));
+    }
+/**
+ * 思路：每次遍历选择最小元素，放到已排序序列的末尾
+ */
+    public int[] Solution_1(int[] nums){
+        int[] res = Arrays.copyOf(nums,nums.length);
+
+        //从头遍历每一个元素与后面未排序元素比较
+        for (int i = 0; i < nums.length - 1; i++){
+            int min = i;
+            
+            // 遍历方式找出未排序序列中的最小值的坐标
+            for (int j = i+1; j < nums.length - 1; j++){
+                if (res[j] < res[min]){
+                    min = j;
+                }
+            }
+
+            // 最小值与res[i]交换位置
+            if (i != min){
+                int tmp = res[i];
+                res[i] = res[min];
+                res[min] = tmp;
+            }
+        }
+        return res;
+    }
 }
