4040e633e80bdc8b7cfa2e08b304832df3a66bd2
shadow <>
2022-02-15 21:05:21 +0800

    回文数

--- src/main/com/shawood/daily/PalindromeNumber.java
+++ src/main/com/shawood/daily/PalindromeNumber.java
@@ -3,19 +3,29 @@ package main.com.shawood.daily;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @author shawood
+ */
 public class PalindromeNumber {
     public static void main(String[] args) {
-        Integer num = -100;
-        String s = String.valueOf(num);
-        char[] c = s.toCharArray();
-        for (int i = 0;i < c.length / 2;i ++){
 
-        }
     }
 }
 
 class Solution {
     public boolean isPalindrome(int x) {
-        return false;
+//        特殊情况
+//        负数不是回文数
+        if (x < 0 || x % 10 == 0 && x != 0 ){
+            return false;
+        }
+
+        int revertedNumber = 0;
+
+        while (x > revertedNumber){
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x = x / 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
